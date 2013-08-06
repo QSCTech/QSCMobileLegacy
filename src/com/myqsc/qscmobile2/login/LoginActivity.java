1625dfbdbd1c5880978403ced4485216de1821ce
richard <richard9372@gmail.com>
2013-08-06 19:28:12 +0800

    add a simple loading animation

--- src/com/myqsc/qscmobile2/login/LoginActivity.java
+++ src/com/myqsc/qscmobile2/login/LoginActivity.java
@@ -1,21 +1,27 @@
 package com.myqsc.qscmobile2.login;
 
 import com.myqsc.qscmobile2.R;
+import com.myqsc.qscmobile2.common.view.LoadingFragment;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Message;
 import android.annotation.SuppressLint;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends FragmentActivity {
 	EditText uid = null;
 	EditText pwd = null;
 	Button btn = null;
@@ -37,23 +43,67 @@ public class LoginActivity extends Activity {
 			@SuppressLint("NewApi")
 			@Override
 			public void onClick(View v) {
+				
+				final View frame = findViewById(R.id.login_activity_frame);
+				
+				final RelativeLayout layout = (RelativeLayout) findViewById(R.id.login_acitivity_main_layout);
+				final Animation animation = AnimationUtils.loadAnimation(getBaseContext(), android.R.anim.fade_out);
+				animation.setAnimationListener(new Animation.AnimationListener() {
+					@Override
+					public void onAnimationStart(Animation animation) {
+					}
+					@Override
+					public void onAnimationRepeat(Animation animation) {
+					}
+					@Override
+					public void onAnimationEnd(Animation animation) {
+						layout.setVisibility(View.GONE);
+						frame.setVisibility(View.VISIBLE);
+					}
+				});
+				layout.startAnimation(animation);
+				
+				final FragmentManager manager = getSupportFragmentManager();
+				final FragmentTransaction transaction = manager.beginTransaction();
+				transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+				final LoadingFragment loadingFragment = new LoadingFragment();
+				transaction.replace(R.id.login_activity_frame, loadingFragment);
+				transaction.commit();
+				
 				CheckUserAsyncTask task = new CheckUserAsyncTask(uid.getText().toString(), pwd.getText().toString(), getBaseContext()) {
 					
 					@Override
 					public void onHandleMessage(Message message) {
 						if (message.what == 0){
-							Toast.makeText(mContext, (CharSequence) message.obj, Toast.LENGTH_SHORT).show();
+							Toast.makeText(mContext, (CharSequence) message.obj, Toast.LENGTH_LONG).show();
 						}
 						if (message.what == 1){
-							Toast.makeText(mContext, "登陆成功", Toast.LENGTH_SHORT).show();
+							Toast.makeText(mContext, "登陆成功", Toast.LENGTH_LONG).show();
 						}
+						
+						final Animation showAnimation = AnimationUtils.loadAnimation(getBaseContext(), android.R.anim.fade_in);
+						showAnimation.setAnimationListener(new Animation.AnimationListener() {
+							@Override
+							public void onAnimationStart(Animation animation) {
+							}
+							@Override
+							public void onAnimationRepeat(Animation animation) {
+							}
+							@Override
+							public void onAnimationEnd(Animation animation) {
+								layout.setVisibility(View.VISIBLE);
+								frame.setVisibility(View.GONE);
+							}
+						});
+						layout.startAnimation(showAnimation);
 					}
 				};
 				if (android.os.Build.VERSION.SDK_INT >= 15){
 					task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 				} else {
 					task.execute();
-				}
+				};
+				
 			}
 		});
 		
