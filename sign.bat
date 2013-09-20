4a7344d235981e5de682a49ccbec4c49e84a2961
richard <richard9372@gmail.com>
2013-09-20 10:45:00 +0800

    发布第27个测试版，使用发行版本签名与编译配置

--- sign.bat
+++ sign.bat
@@ -1,1 +1,5 @@
 jarsigner -keystore "qscmobile2\cert\keystore" -keypass "iloveqsctech" -signedjar "qscmobile2\build\apk\qscmobile2-debug-signed.apk" "qscmobile2\build\apk\qscmobile2-debug-unaligned.apk" "qscdebug"
+zipalign -v 4 "qscmobile2\build\apk\qscmobile2-debug-signed.apk" "qscmobile2\build\apk\qscmobile2-debug.apk"
+
+jarsigner -keystore "qscmobile2\cert\keystore" -keypass "iloveqsctech" -signedjar "qscmobile2\build\apk\qscmobile2-release-signed.apk" "qscmobile2\build\apk\qscmobile2-release-unsigned.apk" "release"
+zipalign -v 4 "qscmobile2\build\apk\qscmobile2-release-signed.apk" "qscmobile2\build\apk\qscmobile2-release.apk"
