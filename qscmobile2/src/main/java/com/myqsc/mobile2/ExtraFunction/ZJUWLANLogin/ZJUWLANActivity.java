9bc976d8fcee4ccd132f01b50ef2536a6d5e1c80
richard <richard9372@gmail.com>
2013-09-29 16:03:01 +0800

    修正若干bug

---
+++ qscmobile2/src/main/java/com/myqsc/mobile2/ExtraFunction/ZJUWLANLogin/ZJUWLANActivity.java
@@ -0,0 +1,16 @@
+package com.myqsc.mobile2.ExtraFunction.ZJUWLANLogin;
+
+import android.os.Bundle;
+
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+
+/**
+ * Created by richard on 13-9-28.
+ */
+public class ZJUWLANActivity extends SwipeBackActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
