664613a5212e7ae03b08635937c1bca3269efb58
Zhang Hai <Dreaming.in.Code.ZH@Gmail.com>
2013-10-21 00:39:13 +0800

    Add basic files for AppWidget of Timetable

---
+++ qscmobile2/src/main/java/com/myqsc/mobile2/Timetable/AppWidget/Provider.java
@@ -0,0 +1,12 @@
+package com.myqsc.mobile2.Timetable.AppWidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class Provider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // TODO
+    }
+}
