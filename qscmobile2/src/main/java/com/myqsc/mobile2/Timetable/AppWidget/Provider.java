3748ea1cc44ca680cc0c8e97b9d44c5dc5208978
Zhang Hai <Dreaming.in.Code.ZH@Gmail.com>
2013-10-27 21:07:53 +0800

    Widget prototype; rename color.xml to colors.xml.

--- qscmobile2/src/main/java/com/myqsc/mobile2/Timetable/AppWidget/Provider.java
+++ qscmobile2/src/main/java/com/myqsc/mobile2/Timetable/AppWidget/Provider.java
@@ -3,10 +3,34 @@ package com.myqsc.mobile2.Timetable.AppWidget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.myqsc.mobile2.R;
 
 public class Provider extends AppWidgetProvider {
+
+    // TODO: Receive data update broadcast
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // TODO
+        RemoteViews appWidgetViews = new RemoteViews(context.getPackageName(), R.layout.appwidget_timetable);
+        appWidgetManager.updateAppWidget(appWidgetIds, appWidgetViews);
     }
+
+    // TODO: Add alarm service?
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+
 }
