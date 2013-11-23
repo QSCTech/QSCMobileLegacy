d5ac78db8e777d756d35998fcb78054b9dd1c4c6
richard <richard9372@gmail.com>
2013-11-23 12:33:11 +0800

    课表卡片倒计时在onpause后暂停

--- qscmobile2/src/main/java/com/myqsc/mobile2/curriculum/fragment/KebiaoCardFragment.java
+++ qscmobile2/src/main/java/com/myqsc/mobile2/curriculum/fragment/KebiaoCardFragment.java
@@ -54,6 +54,19 @@ public class KebiaoCardFragment extends Fragment {
             setTime(diffTime, kebiao);
         }
     };
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        handler.post(runnable);
+    }
+
     @Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -76,15 +89,12 @@ public class KebiaoCardFragment extends Fragment {
         });
 
         helper = new KebiaoDataHelper(getActivity());
-
-        handler.post(runnable);
 		return view;
 	}
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        handler.removeCallbacks(runnable);
     }
 
     private void setTime(int diff, KebiaoClassData kebiaoClassData){
@@ -99,10 +109,5 @@ public class KebiaoCardFragment extends Fragment {
             noticeTextView.setText("距离上课还有");
             diffTextView.setText(Utility.processDiffSecond(-diff));
         }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
     }
 }
