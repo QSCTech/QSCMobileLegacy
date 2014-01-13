d7d7f0458373e4ab7f3a472569527645f20205de
Zhang Hai <Dreaming.in.Code.ZH@Gmail.com>
2014-01-13 19:28:59 +0800

    Add 3x2 AppWidget; Fix 3x3 preview; Update version code.

---
+++ qscmobile2/src/main/java/com/myqsc/mobile2/Timetable/AppWidget/Legacy3x2Provider.java
@@ -0,0 +1,12 @@
+package com.myqsc.mobile2.Timetable.AppWidget;
+
+public class Legacy3x2Provider extends LegacyProvider {
+
+    protected int getDisplayedDateCount() {
+        return 3;
+    }
+
+    protected int getDisplayedTaskCount() {
+        return 3;
+    }
+}
