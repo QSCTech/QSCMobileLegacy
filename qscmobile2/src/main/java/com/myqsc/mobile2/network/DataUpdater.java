2114af8c54122c5c17bf52e6724066f1c3914dbb
Richard <richard9372@gmail.com>
2014-03-26 17:40:12 +0800

    删除一个没用的函数

--- qscmobile2/src/main/java/com/myqsc/mobile2/network/DataUpdater.java
+++ qscmobile2/src/main/java/com/myqsc/mobile2/network/DataUpdater.java
@@ -125,13 +125,4 @@ public class DataUpdater {
         }
         return result.toString();
     }
-
-    // Reads an InputStream and converts it to a String.
-    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-        Reader reader = null;
-        reader = new InputStreamReader(stream, "UTF-8");
-        char[] buffer = new char[len];
-        reader.read(buffer);
-        return new String(buffer);
-    }
 }
