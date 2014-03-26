38d4e3247919c2b5c177d3440288f1034fbd1afe
Zhang Hai <Dreaming.in.Code.ZH@Gmail.com>
2014-03-26 21:20:33 +0800

    Add 4x2 & 4x3 AppWidgets.

---
+++ qscmobile2/src/main/java/com/myqsc/mobile2/Timetable/AppWidget/Legacy4x3Provider.java
@@ -0,0 +1,12 @@
+package com.myqsc.mobile2.Timetable.AppWidget;
+
+public class Legacy4x3Provider extends LegacyProvider {
+
+    protected int getDisplayedDateCount() {
+        return 5;
+    }
+
+    protected int getDisplayedTaskCount() {
+        return 5;
+    }
+}
