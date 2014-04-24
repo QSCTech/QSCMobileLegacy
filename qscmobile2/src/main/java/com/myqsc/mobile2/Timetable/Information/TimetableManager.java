fe0858d0da58d2d295c212e372099be827170557
Zhang Hai <Dreaming.in.Code.ZH@Gmail.com>
2014-04-24 14:00:16 +0800

    Remove cache.

--- qscmobile2/src/main/java/com/myqsc/mobile2/Timetable/Information/TimetableManager.java
+++ qscmobile2/src/main/java/com/myqsc/mobile2/Timetable/Information/TimetableManager.java
@@ -1,10 +1,6 @@
 package com.myqsc.mobile2.Timetable.Information;
 
 import android.content.Context;
-import android.support.v4.util.LruCache;
-
-import com.myqsc.mobile2.Utility.TimeUtils;
-import com.myqsc.mobile2.uti.LogHelper;
 
 import java.util.Calendar;
 import java.util.Collections;
@@ -17,8 +13,6 @@ import java.util.TreeSet;
 // TODO: Invalidate the cache by cache.evictAll() when changes are made.
 public class TimetableManager {
 
-    private static final int CACHE_SIZE = 3;
-
     private static final Object instanceLock = new Object();
 
     private static TimetableManager instance = null;
@@ -26,9 +20,6 @@ public class TimetableManager {
     private Context context;
 
     private Set<TaskProvider> taskProviders = Collections.synchronizedSet(new LinkedHashSet<TaskProvider>());
-
-    // NOTE: java.util.WeakHashMap holds weak references of keys, not values.
-    private final LruCache<Calendar, SortedSet<Task>> cache = new LruCache<Calendar, SortedSet<Task>>(CACHE_SIZE);
 
 
     private TimetableManager(Context context) {
@@ -58,31 +49,10 @@ public class TimetableManager {
 
     public SortedSet<Task> getTimetable(Calendar date) {
 
-        // REMOVEME: Debug statement.
-        Calendar dateCopy = (Calendar) date.clone();
-        TimeUtils.clearTime(dateCopy);
-        if (!date.equals(dateCopy)) {
-            throw new RuntimeException("Should use a date with time cleared here.");
-        }
+        SortedSet<Task> timetable = Collections.synchronizedSortedSet(new TreeSet<Task>());
 
-        SortedSet<Task> timetable;
-
-        synchronized (cache) {
-
-            timetable = cache.get(date);
-
-            if (timetable == null) {
-
-                timetable = Collections.synchronizedSortedSet(new TreeSet<Task>());
-
-                for (TaskProvider taskProvider : taskProviders) {
-                    timetable.addAll(taskProvider.getTasks(date));
-                }
-
-                cache.put(date, timetable);
-            }
-
-            LogHelper.i(cache.toString());
+        for (TaskProvider taskProvider : taskProviders) {
+            timetable.addAll(taskProvider.getTasks(date));
         }
 
         return timetable;
